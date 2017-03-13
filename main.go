f43247b6511d1a44b36281809fa6696bc32ddbf6
Rohan Allison <rohanthewiz_at_gmail.com>
2017-03-13 00:37:26 -0500

    Begin adding auth

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ import (
 	//. "github.com/vattle/sqlboiler/queries/qm"
 	"fmt"
 	"encoding/json"
+	"net/http"
+	"time"
 )
 
 func main() {
@@ -25,6 +27,13 @@ func main() {
 
 	e := echo.New()
 
+	admin := e.Group("/admin", RoAuth)
+//	or: admin.Use(RoAuth)
+	admin.GET("/", func(c echo.Context) error{
+		c.String(200, "Hello Admin!")
+		return nil
+	})
+
 	e.GET("/", func(c echo.Context) (err error) {
 		buf := new(bytes.Buffer)
 		template.UserList(pilotsList, buf)
@@ -34,6 +43,31 @@ func main() {
 		//resp.Write([]byte("Hello World"))
 		//fmt.Fprint(c.Response(), "Hello World!")
 		return
+	})
+	e.GET("/login", func(c echo.Context) error {
+		c.String(http.StatusOK, "We would login here")
+
+		return nil
+	})
+
+	// Simulate a good login
+	e.GET("/auth", func(c echo.Context) error {
+		fmt.Println("In /auth...")
+		sess, err := c.Cookie("sess_id")
+		if err != nil || sess.Value == "" {
+			cookie := new(http.Cookie)
+			cookie.Name = "sess_id"
+			cookie.Value = "xY78jq"
+			cookie.Expires = time.Now().Add(24 * time.Hour)
+			c.SetCookie(cookie)
+			fmt.Println("Set auth cookie")
+
+			// todo create a key in Redis with a TTL (https://github.com/go-redis/redis)
+			// Perhaps use a redis hash
+			// key: { user_id: "nnn", first_name: "john", last_name: "Smith" }
+		}
+		// todo for normal login: https://godoc.org/golang.org/x/crypto/scrypt
+		return nil
 	})
 
 	e.GET("/pilots", func(c echo.Context) (error) {
@@ -96,6 +130,47 @@ func main() {
 		return err
 	})
 
-
 	e.Start(":3000")
+}
+
+// Auth middleware
+func RoAuth(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// Get session_id from cookie
+		sess, err := c.Cookie("sess_id")
+		if err != nil {
+			redirect_to_login(c)
+			return nil
+		}
+		if sess.Value == "" { redirect_to_login(c) }
+
+		// Retrieve session from store
+		session, err := get_session(sess.Value)
+		if err != nil {
+			redirect_to_login(c)
+			return nil
+		}
+		// If session.blank?: redirect to login
+		extend_session(session, 3600)
+		return next(c)
+	}
+}
+
+// todo
+func get_session(session_id string) (map[string]string, error) {
+	var err error
+	fmt.Println("Getting session for:", session_id)
+	session := make(map[string]string)  // bogus
+
+	return session, err
+}
+
+// todo
+func extend_session(session map[string]string, ext int64) {
+	fmt.Println("Extending session:", session, "by", ext, "seconds")
+	// todo extend TTL of key in Redis
+}
+
+func redirect_to_login(c echo.Context) {
+	c.Redirect(http.StatusTemporaryRedirect, "/login")
 }
